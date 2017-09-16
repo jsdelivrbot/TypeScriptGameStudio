1a0d237be02ad16784fa6bafd1380207ee7381c2
Maximilian Hasselbusch <MaxHasselbusch@dyn134141.cc.lehigh.edu>
2017-09-16 14:26:17 -0400

    test

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/test'));
 
 app.get('/', function(request, response) {
 	response.sendFile(path.resolve('test/test.html'));
-})
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
