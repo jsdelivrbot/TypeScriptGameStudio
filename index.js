8d94acb2117f97d1e661353892a93c25a892f302
Maximilian Hasselbusch <MaxHasselbusch@dyn134141.cc.lehigh.edu>
2017-09-16 13:39:21 -0400

    test

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-var express = require('express')
-var app = express()
+var http = require('http');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+}).listen(8080);
