bda1d41c7eae268ad24dd341f5cccb452cd5312d
MicahFive <mic415@lehigh.edu>
2017-10-13 11:07:08 -0400

    took classes out of testclass.ts

--- library/testclass.ts
+++ library/testclass.ts
@@ -24,68 +24,11 @@ PIXI.loader
 .add(rightImg)
 .load(()=>main(20));
 
-class Lol {
-  mManager: LolManager;
 
-  constructor(manager: LolManager) {
-    this.mManager = manager;
-  }
 
-  render() {
-    this.mManager.mWorld.mWorld.Step(1/45, 8, 3);
-    this.mManager.mWorld.mCamera.updatePosition();
-    this.mManager.mWorld.render();
-    this.mManager.mHud.render();
-    renderer.render(this.mManager.mContainer);
-  }
-}
 
-class LolManager {
-  mWorld: MainScene;
-  mHud: Scene;
-  mContainer: PIXI.Container;
 
-  constructor(world: MainScene, hud?: Scene) {
-    this.mWorld = world;
-    if (hud) this.mHud = hud;
-    this.mContainer = new PIXI.Container();
-    this.mContainer.addChild(this.mWorld.mCamera.mContainer);
-    if (hud) this.mContainer.addChild(hud.mContainer);
-  }
-}
 
-// TODO: Right now the camera can only follow an actor
-class Camera {
-  mContainer: PIXI.Container;
-  mScene: Scene;
-  mChaseActor: Actor;
-
-  constructor(chaseActor: Actor) {
-    this.mContainer = new PIXI.Container();
-    this.mChaseActor = chaseActor;
-  }
-
-  changeScene(scene: Scene) {
-    this.mContainer.removeChildren();
-    this.mScene = scene;
-    this.mContainer.addChild(scene.mContainer);
-  }
-
-  updatePosition() {
-    this.mContainer.pivot = this.mChaseActor.mSprite.position;
-    this.mContainer.position.x = renderer.width / 2;
-    this.mContainer.position.y = renderer.height / 2;
-  }
-
-  setZoom(x: number, y: number) {
-    this.mContainer.scale.set(x, y);
-  }
-
-  zoomInOut(x: number, y:number) {
-    let z = this.mContainer.scale;
-    this.mContainer.scale.set(z.x * x, z.y * y);
-  }
-}
 
 class Actor {
   mSize: PhysicsType2d.Vector2;
