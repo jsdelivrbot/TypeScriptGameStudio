50be8772fc4f01a555b4942702bb9f2f836ce31e
MicahFive <mic415@lehigh.edu>
2017-11-16 12:52:18 -0500

    test

--- library/testclass.js
+++ library/testclass.js
@@ -2178,7 +2178,7 @@ class Level {
     * @param color The color, formatted as a hex number
     */
     setBackgroundColor(color) {
-        this.mGame.mRenderer.options.backgroundColor = color;
+        this.mGame.mRenderer.backgroundColor = color;
     }
     //                                 /**
     //                                 * Add a picture that may repeat in the X dimension
