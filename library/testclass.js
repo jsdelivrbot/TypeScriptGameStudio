55989a3feb66c82d5c5f82dc12be623e49b7135d
MicahFive <mic415@lehigh.edu>
2017-11-30 17:33:25 -0500

    testing

--- library/testclass.js
+++ library/testclass.js
@@ -5275,6 +5275,7 @@ class Route {
             this.mRouteVelocity = velocity;
             this.mRouteLoop = loop;
             this.mActor = actor;
+            this.mRouteVec = new PhysicsType2d.Vector2(0, 0);
             // kick off the route, indicate that we aren't all done yet
             this.startRoute();
             this.mRouteDone = false;
