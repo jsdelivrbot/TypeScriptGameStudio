88ec860534910b25b7fceba18a60d925588dbec8
MicahFive <mic415@lehigh.edu>
2017-12-02 23:56:01 -0500

    testing

--- library/Level.ts
+++ library/Level.ts
@@ -699,7 +699,6 @@ class Level {
         let v = actor.mBody.GetLinearVelocity();
         v.x = xRate;
         actor.updateVelocity(v.x, v.y);
-        actor.mBody.SetLinearDamping(5);
       }
     })();
   }
--- library/testclass.js
+++ library/testclass.js
@@ -1441,7 +1441,7 @@ class Level {
                 let v = actor.mBody.GetLinearVelocity();
                 v.x = xRate;
                 actor.updateVelocity(v.x, v.y);
-                actor.mBody.SetLinearDamping(100);
+                actor.mBody.SetLinearDamping(5);
             }
         })();
     }
