b1a8d4d6c0b17119ca551f3b85727d3a5d7c1d3f
MicahFive <mic415@lehigh.edu>
2017-12-04 19:57:55 -0500

    chooser

--- library/testclass.js
+++ library/testclass.js
@@ -5540,7 +5540,7 @@ class Chooser {
             let midX = 960 / 2;
             let midY = 540 / 2;
             // Back to splash
-            level.addStaticTextCentered(midX, midY + 50, "Arial", 0x00FFFF, 24, "Back to Menu", 1);
+            level.addStaticTextCentered(midX, midY + 50, "Arial", 0x00FFFF, 24, "Menu", 1);
             level.addTapControl(midX - 50, midY + 100, 100, 50, "./GameAssets/button.png", new (class _ extends LolAction {
                 go() {
                     level.doSplash();
@@ -5549,7 +5549,7 @@ class Chooser {
             })());
             // Play level 1 button
             // Colors are written in hex values
-            level.addStaticText(50, midY - 150, "Arial", 0x00FFFF, 24, "Play Sky Fighter", 1);
+            level.addStaticTextCentered(50, midY - 150, "Arial", 0x00FFFF, 24, "Sky Fighter", 1);
             level.addTapControl(50, midY - 100, 200, 50, "./GameAssets/button.png", new (class _ extends LolAction {
                 go() {
                     level.doLevel(1);
@@ -5557,21 +5557,22 @@ class Chooser {
                 }
             })());
             // Play level 2 button
-            level.addStaticText(midX - 100, midY - 150, "Arial", 0x00FFFF, 24, "Play Christmas Scramble", 1);
+            level.addStaticTextCentered(midX - 100, midY - 150, "Arial", 0x00FFFF, 24, "Christmas Scramble", 1);
             level.addTapControl(midX - 100, midY - 100, 200, 50, "./GameAssets/button.png", new (class _ extends LolAction {
                 go() {
                     level.doLevel(2);
                     return true;
                 }
             })());
-            // Play level 3 tebutton
-            level.addStaticText(960 - 250, midY - 150, "Arial", 0x00FFFF, 24, "Play Dodgy Plane", 1);
-            level.addTapControl(960 - 250, midY - 100, 200, 50, "./GameAssets/button.png", new (class _ extends LolAction {
+            // Play level 3 button
+            level.addStaticTextCentered(960 - 250, midY - 100, "Arial", 0x00FFFF, 24, "Dodgy Plane", 1);
+            level.addTapControl(960 - 250, midY - 100, 200, 50, "", new (class _ extends LolAction {
                 go() {
                     level.doLevel(3);
                     return true;
                 }
             })());
+            level.drawPicture(960 - 250, midY - 100, 200, 50, "./GameAssets/button.png", -1);
         }
     }
 }
